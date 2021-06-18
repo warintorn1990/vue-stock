aa7ef5328a257d0815e1b4bd12c3b41be96513d3
Warintorn Wiboonsin <warintorn.wiboonsild@gmail.com>
2021-06-18 16:37:47 +0700

    fix chart

--- src/components/charts/LineChart.vue
+++ src/components/charts/LineChart.vue
@@ -1,11 +1,38 @@
 <script>
-import { Line, mixins } from 'vue-chartjs'
+import { Line, mixins } from "vue-chartjs";
 const { reactiveProp } = mixins
 
 export default {
   extends: Line,
   mixins: [reactiveProp],
-  props: ['options'],
+  data() {
+    return {
+      options: {
+        legend: {
+          display: true
+        },
+        maintainAspectRatio: false,
+         scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+                callback: function (value, index, values) {
+                return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+              }
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [ {
+            gridLines: {
+              display: true
+            }
+          }]
+        },
+      }
+    }
+  }, 
   mounted () {
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="small">
-    <line-chart :chart-data="datacollection"></line-chart>
-    <button @click="fillData()">Randomize</button>
-  </div>
+  <v-container>
+    <v-card>
+      <v-btn class="success" @click="fillData()">Refresh</v-btn>
+      <LineChart style="height: 300px;" :chartData="datacollection" />
+      <BarChart style="height: 300px;"  :chartData="datacollection" />
+    </v-card>
+  </v-container>
 </template>
 
 <script>
   import LineChart from '@/components/charts/LineChart'
+  import BarChart from '@/components/charts/BarChart'
+
 
   export default {
     components: {
-      LineChart
+      LineChart,
+      BarChart
     },
     data () {
       return {
@@ -21,24 +27,73 @@
       this.fillData()
     },
     methods: {
-      fillData () {
-        this.datacollection = {
-          labels: [this.getRandomInt(), this.getRandomInt()],
+    fillData () {
+      this.datacollection = {
+          //Data to be represented on x-axis
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
           datasets: [
             {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }, {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }
-          ]
-        }
+              label: '2019',
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: '#249EBF',
+              data: this.getRandomInt(),
+
+              borderColor: [
+                '#43A047'
+               ],
+               borderWidth: 2,
+               backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                 'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            },
+
+              {
+              label: '2019',
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: '#249EBF',
+              data: this.getRandomInt(),
+
+              borderColor: [
+                '#ff0000'
+               ],
+               borderWidth: 2,
+               backgroundColor: [
+                 'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',                
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                 'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            },
+          ]};
+          
       },
       getRandomInt () {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+        let randoms = []
+        for (let index = 0; index < 12; index++) {
+          randoms.push(Math.floor(Math.random() * (50000 - 5 + 1)) + 5)
+        }
+        return randoms
       }
     }
   }
